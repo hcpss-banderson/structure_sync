168473790c61fccc9d702509cbfa31e9f7e132ca
Tim Kruijsen <tim.kruijsen@ordina.nl>
2017-06-26 16:38:26 +0200

    [MODULE] Adds query to get all taxonomies

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,8 +3,6 @@
 namespace Drupal\structure_sync\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
-use Drupal\Core\Link;
-use Drupal\Core\Url;
 
 /**
  * Controller routines for taxonomy_sync routes.
@@ -21,22 +19,45 @@ class MainController extends ControllerBase {
     $vocabulary_list = array();
     $vocabularies = $this->entityTypeManager()->getStorage('taxonomy_vocabulary')->loadMultiple();
     foreach ($vocabularies as $vocabulary) {
-//      $vocabulary_form = Url::fromRoute('taxonomy_manager.admin_vocabulary',
-//        array('taxonomy_vocabulary' => $vocabulary->id()));
-//      $vocabulary_list[] = Link::fromTextAndUrl($vocabulary->id(), $vocabulary_form);
       $vocabulary_list[] = $vocabulary->id();
     }
     if (!count($vocabulary_list)) {
-      $vocabulary_list[] = array('#markup' => $this->t('No vocabularies available'));
+      $vocabulary_list[] = ['#markup' => $this->t('No vocabularies available')];
     }
 
     $build['vocabularies'] = [
       '#markup' => '<h2>' . $this->t('Vocabularies') . '</h2>',
     ];
 
+    // TODO: Export button.
+
     foreach ($vocabulary_list as $vocabulary) {
+      $query = \Drupal::database()->select('taxonomy_term_field_data', 'ttfd');
+      $query->fields('ttfd', [
+        'tid',
+        'name',
+        'langcode',
+        'description__value',
+        'description__format',
+        'weight',
+        'changed',
+        'default_langcode',
+      ]);
+      $query->addField('tth', 'parent');
+      $query->join('taxonomy_term_hierarchy', 'tth', 'ttfd.tid = tth.tid');
+      $query->addField('ttd', 'uuid');
+      $query->join('taxonomy_term_data', 'ttd', 'ttfd.tid = ttd.tid');
+      $query->condition('ttfd.vid', $vocabulary);
+      $tags = $query->execute()->fetchAll();
+
+      $listItems = '';
+      foreach ($tags as $item) {
+        $listItems .= '<li>' . $item->parent . ' - ' . $item->tid . ': ' . $item->name . ' (' . $item->uuid . ')</li>';
+      }
+
       $build['vocabularies'][] = [
-        '#markup' => '<p><h3 class="button">' . $vocabulary . '</h3></p>',
+        '#markup' => '<p><h3>' . $vocabulary . '</h3>
+                      <ul>' . $listItems . '</ul></p>',
       ];
     }
 
